79c48ab49565ce2b8be161aedbfc10d844b8a887
alexander-p30 <alexanderp30e@gmail.com>
2023-04-22 16:46:28 -0300

    change a bunch of stuff for macOS

--- dot_config/nvim/lua/config/init.lua
+++ dot_config/nvim/lua/config/init.lua
@@ -3,5 +3,5 @@ require('config.lazy')
 require('config.autocmds')
 require('config.keymap')
 
-vim.cmd.colorscheme('catppuccin')
+vim.cmd.colorscheme('tokyonight')
 vim.cmd.highlight('LineNr guifg=#9ca0a4')
